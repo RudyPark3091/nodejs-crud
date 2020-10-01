32f73ead51cf6c753fe75b1d158b6843f6cdc062
RudyPark3091 <grayblack313@gmail.com>
2020-10-02 01:53:30 +0900

    Add index router module
    app.use(express.json()) -> allows access in req.body of POST method

--- src/index.js
+++ src/index.js
@@ -5,15 +5,17 @@ const fs = require("fs");
 
 const PORT_NUM = 3000;
 
+const indexRouter = require("./routes/index");
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set('views', __dirname + '/view');
 app.set('view engine', 'ejs');
 app.engine('html', require('ejs').renderFile);
 
-app.get("/", (req, res) => {
-  res.render('index', { title: 'BOARD SERVICE' });
-});
+app.use('/', indexRouter);
 
 app.listen(PORT_NUM, () => {
   console.log("server ready");
