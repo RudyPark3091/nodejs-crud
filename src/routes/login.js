d6e07823edda951e10c3ada3b5a5c4a03a5577c6
RudyPark3091 <grayblack313@gmail.com>
2020-11-17 01:49:53 +0900

    Add Sample user to db

--- src/routes/login.js
+++ src/routes/login.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { sequelize } = require("../model/model.js");
 const Posts = require("../model/posts.js");
+const Users = require("../model/users.js");
 const cookieParser = require("cookie-parser");
 
 router.use('/', (req, res, next) => {
@@ -11,6 +12,23 @@ router.use('/', (req, res, next) => {
 
   // TODO: login success -> redirect to index page
   // login failure -> alert and remain on current page
+  next();
+});
+
+router.get('/', (req, res, next) => {
+  Users.findOne({where: { alias: "SampleUser" }}).then(user => {
+    if (user) {
+      // sample user already exists
+    } else {
+      Users.create({
+        email: "asdf@gmail.com",
+        password: "asdfasdf",
+        alias: "SampleUser"
+      });
+    }
+  }).catch(err => {
+    console.error(err);
+  });
 });
 
 module.exports = router;
