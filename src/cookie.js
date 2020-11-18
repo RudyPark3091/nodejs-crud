7a73552e96ad4affd2131827cd2a4d0c0f6013da
RudyPark3091 <grayblack313@gmail.com>
2020-11-19 01:36:51 +0900

    Remove useless file

--- src/cookie.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-
-http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Set-Cookie': ['firstCookie=cookie!', 'secondCookie=ck!']
-  });
-  res.end('Hello World');
-}).listen(3000);
